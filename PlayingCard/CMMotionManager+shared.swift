161dc295afa37ed621674712177f81c9e5f527a9
Mila B <milabochkova03@gmail.com>
2022-12-27 16:15:14 +0300

    add gravity behavior

---
+++ PlayingCard/CMMotionManager+shared.swift
@@ -0,0 +1,12 @@
+//
+//  CMMotionManager+shared.swift
+//  PlayingCard
+//
+//  Created by Mila B on 27.12.2022.
+//
+
+import CoreMotion
+
+extension CMMotionManager {
+    static var shared = CMMotionManager()
+}
